6f9fe5904e36891430437436a1e5cf227404a7c9
ASHA_MAITY <ASHA_MAITY@DESKTOP-07N31E3>
2019-06-29 11:22:05 +0530

    MyThird commit

--- B18CucuRestAssured/src/test/java/com/basic/testinggoogle/TestingGoogleWebsite.java
+++ B18CucuRestAssured/src/test/java/com/basic/testinggoogle/TestingGoogleWebsite.java
@@ -10,18 +10,18 @@ import io.restassured.response.Response;
 public class TestingGoogleWebsite {
 	
 	
-//	public static void main(String[] args) {
-//	Response res=RestAssured.given().get("https://www.google.com/");
-//		int startCode=res.statusCode();
-//		System.out.println("Status Code"+startCode);
-//	}
-
-
-	public void googleTest() {
-		
-		io.restassured.response.Response res=RestAssured.given().get("https://www.google.com/");
+	public static void main(String[] args) {
+	Response res=RestAssured.given().get("https://www.google.com/");
 		int startCode=res.statusCode();
 		System.out.println("Status Code"+startCode);
-		Assert.assertEquals(startCode,200);
 	}
+
+
+//	public void googleTest() {
+//		
+//		io.restassured.response.Response res=RestAssured.given().get("https://www.google.com/");
+//		int startCode=res.statusCode();
+//		System.out.println("Status Code"+startCode);
+//		Assert.assertEquals(startCode,200);
+//	}
 }
